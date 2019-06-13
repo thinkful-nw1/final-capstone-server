a5fa067dcc73613622460b546f29ae3f88bd322d
Sheba Mathew <sheba.m.mathew@gmail.com>
2019-06-13 19:24:57 -0400

    changes to seed files

--- seeds/seed.travelapp_tables.sql
+++ seeds/seed.travelapp_tables.sql
@@ -6,8 +6,7 @@ TRUNCATE
 
 INSERT INTO users (email, username, password) 
   VALUES
-  ('testuser@gmail.com', 'testuser', '$2a$12$QUHCYSD.c0cGyHMzgrGyl.1v4hV0Th0NXGZLGpqdJeAhWzgisb6MG'),
+  ('testuser@gmail.com', 'testuser', '$2a$12$VHj4PeIF3XGyU3cOpUk0KuDRxo1qa2Hqwk5VWl8jIl5wWpmK3Py66'),
   ('sheba.m.mathew@gmail.com', 'shebamathew',  '$2a$12$fx4BFJnLVKRnVWGgdLFHx.oCj5abDrjAuZQcVhIPm8KG0ih8UkDg2'),
   ('jordanepps@gmail.com', 'jordanepps', '$2a$12$tR.Zs7/TgyzJvmY4mw4oZuzlNG5tzGl2nd7dnN1ihhNa7vfhLDXBe');
-
 COMMIT;
